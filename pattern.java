ce9c3328a0382717fd27d5646800e0cdf178f79c
Abhishek <001abhishek287@gmail.com>
2023-04-05 01:29:44 +0530

    testing the branch case

---
+++ pattern.java
@@ -0,0 +1,12 @@
+class pattern
+{
+    public static void main(String[] args)
+    {
+        int j = 1;
+        while(j<6)
+        {
+            System.out.println("Artificial and robotic planet");
+            j++;
+        }
+    }
+}
