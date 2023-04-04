41d2aec6c4689d9d092400e232a4cf011abad8b3
Abhishek <001abhishek287@gmail.com>
2023-04-04 23:59:00 +0530

    adding two numbers in java with proper message

---
+++ add.java
@@ -0,0 +1,10 @@
+class add
+{
+    public static void main(String[] args)
+    {
+        int a = 24;
+        int b = 12;
+        int result = a + b;
+        System.out.println("The sum of two numbers is " + result)
+    }
+}
