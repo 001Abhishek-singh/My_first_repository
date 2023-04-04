3ecdb980bdcc94df7561cc8c1b866ac9256296bb
Abhishek <001abhishek287@gmail.com>
2023-04-05 00:14:42 +0530

    assigning the if - else condition

---
+++ condition.java
@@ -0,0 +1,11 @@
+class condition
+{
+    public static void main(String[] args)
+    {
+        boolean a = True;
+        if a == True;
+        System.out.println("This is correct: ");
+        else;
+        System.out.println("This is false: ")
+    }    
+}
